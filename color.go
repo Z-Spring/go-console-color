18ae6e51a239a9fb204391ec8388d62bc7296376
murphy <murphyqq1@gmail.com>
2022-07-16 02:04:17 +0800

    feat: add int64 float64 and bool methods

--- color.go
+++ color.go
@@ -1,5 +1,9 @@
 package go_console_color
 
+import (
+	"strconv"
+)
+
 // define output color
 // or if you just want to display the text color
 // just delete 97; || 90;
@@ -17,22 +21,61 @@ const (
 	reset  = "\033[0m"
 )
 
+type color string
+
 const (
-	Green  string = "Green"
-	Red    string = "Red"
-	Cyan   string = "Cyan"
-	Blue   string = "Blue"
-	Yellow string = "Yellow"
-	White  string = "White"
-	Purple string = "Purple"
-	Gray   string = "Gray"
+	Green  color = "Green"
+	Red    color = "Red"
+	Cyan   color = "Cyan"
+	Blue   color = "Blue"
+	Yellow color = "Yellow"
+	White  color = "White"
+	Purple color = "Purple"
+	Gray   color = "Gray"
 )
 
-// ConsoleColor display console's text  background color
-func ConsoleColor(inputColor string, target string) string {
-	suffix := target + reset
+type ConsoleColor struct {
+	C           color
+	CancelColor bool
+}
 
-	switch inputColor {
+func NewColor(c color) *ConsoleColor {
+	return &ConsoleColor{
+		C: c,
+	}
+}
+
+func convert(cs *ConsoleColor, target interface{}) string {
+	var (
+		suffix string
+		text   string
+	)
+
+	switch target := target.(type) {
+	case int64:
+		targetConv := strconv.Itoa(int(target))
+		suffix = targetConv + reset
+		text = targetConv
+	case string:
+		suffix = target + reset
+		text = target
+	case float64:
+		targetConv := strconv.FormatFloat(target, 'E', -1, 64)
+		suffix = targetConv + reset
+		text = targetConv
+
+	case bool:
+		targetConv := strconv.FormatBool(target)
+		suffix = targetConv + reset
+		text = targetConv
+
+	case byte:
+		suffix = string(target) + reset
+		text = string(target)
+
+	}
+
+	switch cs.C {
 	case Red:
 		return red + suffix
 	case Green:
@@ -50,6 +93,50 @@ func ConsoleColor(inputColor string, target string) string {
 	case Gray:
 		return gray + suffix
 	default:
+		return reset + text
+	}
+
+}
+
+// String outputs the target content with color
+func (cs *ConsoleColor) String(target string) string {
+	if cs.CancelColor {
 		return reset + target
+	} else {
+		return convert(cs, target)
+
+	}
+}
+
+func (cs *ConsoleColor) Int64(target int64) string {
+	targetConv := strconv.Itoa(int(target))
+
+	if cs.CancelColor {
+		return reset + targetConv
+	} else {
+		return convert(cs, target)
+
+	}
+}
+
+func (cs *ConsoleColor) Float64(target float64) string {
+	targetConv := strconv.FormatFloat(target, 'E', -1, 64)
+
+	if cs.CancelColor {
+		return reset + targetConv
+	} else {
+		return convert(cs, target)
+
+	}
+}
+
+func (cs *ConsoleColor) Bool(target bool) string {
+	targetConv := strconv.FormatBool(target)
+
+	if cs.CancelColor {
+		return reset + targetConv
+	} else {
+		return convert(cs, target)
+
 	}
 }
--- color_test.go
+++ color_test.go
@@ -5,7 +5,36 @@ import (
 	"testing"
 )
 
-func TestConsoleColor(t *testing.T) {
-	s := ConsoleColor(Cyan, "hello world!")
-	fmt.Println(s)
+func TestConsoleColor_String(t *testing.T) {
+	c := ConsoleColor{
+		C:           Red,
+		CancelColor: true,
+	}
+	p := c.String("spring")
+	if p != reset+"spring" {
+		t.Error("can output spring")
+		return
+	}
+	fmt.Printf("%v\n", p)
+}
+
+func TestConsoleColor_Bool(t *testing.T) {
+	c1 := ConsoleColor{
+		C:           Yellow,
+		CancelColor: true,
+	}
+	p := c1.Bool(true)
+
+	fmt.Printf("%v\n", p)
+
+	c2 := ConsoleColor{
+		C:           Yellow,
+		CancelColor: false,
+	}
+	p2 := c2.Bool(true)
+	if p != reset+"true" {
+		t.Error("can output true")
+		return
+	}
+	fmt.Printf("%v\n", p2)
 }
